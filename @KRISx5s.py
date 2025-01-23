c1aca042f37ead35b6ae44aac355008d53f80890
uday-655 <jiwaj5158@gmail.com>
2025-01-23 08:00:25 +0530

    Update @KRISx5s.py

--- @KRISx5s.py
+++ @KRISx5s.py
@@ -16,7 +16,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = ''
+TOKEN = '7743425789:AAHv3U-QhDB4KX0JCFVoydBANaFOdz39k38'
 MONGO_URI = 'mongodb+srv://Bishal:Bishal@bishal.dffybpx.mongodb.net/?retryWrites=true&w=majority&appName=Bishal'
 FORWARD_CHANNEL_ID = -1002156421934
 CHANNEL_ID = -1002156421934
@@ -28,7 +28,7 @@ client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client['zoya']
 users_collection = db.users
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(7743425789:AAHv3U-QhDB4KX0JCFVoydBANaFOdz39k38)
 REQUEST_INTERVAL = 1
 
 blocked_ports = [8700, 20000, 443, 17500, 9031, 20002, 20001]  # Blocked ports list
